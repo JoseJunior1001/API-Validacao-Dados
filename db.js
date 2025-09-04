71d4e6092d247c0c0b7ec4f60adde66f1b1113cf
JoseJunior1001 <jose.silva2399@etec.sp.gov.br>
2025-09-04 16:43:42 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
-const mysql = require("mysql2");
+const { Pool } = require("pg");
 require("dotenv").config();
 
-const connection = mysql.createPool({
+const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-module.exports = connection;
+module.exports = pool;
