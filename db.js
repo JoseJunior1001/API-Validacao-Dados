de6728ac6093e0ce77237f44b3e0fb991a8068e2
JoseJunior1001 <jose.silva2399@etec.sp.gov.br>
2025-09-04 21:16:17 -0300

    Update db.js

--- db.js
+++ db.js
@@ -2,14 +2,15 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,     
+  user: process.env.DB_USER,      
+  password: process.env.DB_PASSWORD, 
+  database: process.env.DB_NAME, 
+  port: process.env.DB_PORT,     
   ssl: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false    
+  },
+  connectionTimeoutMillis: 5000   
 });
 
 module.exports = pool;
